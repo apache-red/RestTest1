81c312223b740a7c147d05687d86d9edff069b97
apache-red <apache_red@mail.ru>
2019-04-24 08:35:51 +0300

    git init

---
+++ src/main/java/com/redcompany/testRest1/TestRest1Application.java
@@ -0,0 +1,13 @@
+package com.redcompany.testRest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRest1Application.class, args);
+	}
+
+}
